c4ea4c27457bd3aefd9ee3a1f69b32623f5d0d41
samiselim <samiselim75@gmail.com>
2024-01-28 18:47:47 +0200

    delete Return value to dockerLogin method

--- src/sami/packages/Docker.groovy
+++ src/sami/packages/Docker.groovy
@@ -7,10 +7,23 @@ class Docker implements Serializable{
     Docker(script){
          this.script = script
     }
-
+    def extractSecretName(grepOutput) {
+        def matcher = (grepOutput =~ /\bsecret-reg\S*/)
+        if (matcher.find()) {
+            return matcher.group()
+        }
+        return null
+    }
     def dockerLogin(String dockerHubCred){
         script.withCredentials([script.usernamePassword(credentialsId: "${dockerHubCred}", passwordVariable: 'PASS', usernameVariable: 'USER')]){
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            def secretName = extractSecretName(sh(script: "kubectl get secret | grep ${SECRET_NAME_PREFIX}", returnStdout: true).trim())
+            if (secretName) {
+                echo "Found secret: ${secretName}"
+            } else {
+                echo "No matching secret found with the prefix '${SECRET_NAME_PREFIX}'."
+            }
+           
             script.sh "kubectl create secret docker-registry secret-reg \
             --docker-server=docker.io \
             --docker-username=samiselim \
