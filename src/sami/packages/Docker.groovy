67ceae545c448e9a07ba2840a221dc0445cc2c26
samiselim <samiselim75@gmail.com>
2024-01-28 19:35:06 +0200

    delete Return value to dockerLogin method

--- src/sami/packages/Docker.groovy
+++ src/sami/packages/Docker.groovy
@@ -9,8 +9,7 @@ class Docker implements Serializable{
     }
 def dockerLogin(String dockerHubCred) {
     script.withCredentials([script.usernamePassword(credentialsId: "${dockerHubCred}", passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        def name = script.sh(script: 'kubectl get secret | grep secret-reg | awk \'{print $1}\'', returnStatus: true)
-        name = name.trim()
+        def name = script.sh(script: 'kubectl get secret | grep secret-reg | awk \'{print $1}\'', returnStdout: true)
         script.sh "echo $name"
 
         if (name == 'secret-reg') {
